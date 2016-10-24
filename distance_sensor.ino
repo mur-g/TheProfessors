732f9e53b05fc8d7a5de9b78e2d3d919a9875672
mur-g <mlgeorgeson@utexas.edu>
2016-10-24 14:38:29 -0500

    commit two

--- distance_sensor.ino
+++ distance_sensor.ino
@@ -14,10 +14,10 @@ void setup() {
 void loop() {
   int front_speed = analogRead(A7);               // the input from the front sensor connected to analog pin 7
   int front = map(front_speed, 0, 1023, 0, 255);  // scales down the analogRead from max of 1023 to max of 255 compatible with the sensor
-  analogWrite(12, front);                         // pin 12 will have a PWM value of the variable front
+  analogWrite(12, front);                         // writes the input from the first sensor onto pin 12
 
   int back_speed = analogRead(A4);                // the input from the back sensor connected to analog pin 4
   int back = map(back_speed, 0, 1023, 0, 255);    // scales down the analogRead from max of 1023 to max of 255 compatible with the sensor
-  analogWrite(13, back);                          // pin 13 will have a PWM value of the variable back
+  analogWrite(13, back);                          // writes the input from the back sensor onto pin 13
   
 }
