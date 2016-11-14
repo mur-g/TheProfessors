0e706734c5e7c4c45fb72f437fd1259923c89c6a
SamSelle <sselle@utexas.edu>
2016-11-14 15:55:00 -0600

    Add files via upload

--- _1ball_2poke.ino
+++ _1ball_2poke.ino
@@ -15,36 +15,57 @@ Servo right;    // create servo object to control right servo
 Servo left;     // create servo object to control left servo
 Servo gate;     // create servo object to control left servo
 
+// 0 is forwards for right and backwards for left
+// 180 is backwards for right and forwards for left
+
 
 void setup() {
  
   right.attach(24);   // attaches the right servo on pin 23 to the servo object 
   left.attach(23);    // attaches the left servo on pin 24 to the servo object
+  gate.attach(25);
+}
+
+/**/
+void dropGate(){
+  
+}
+
+/*
+  Walks forward for half a second
+  to begin line follow
+*/
+void start(){
+  
 }
 
 void loop() {
 
-  analogLightRight = analogRead(A1);    // attaches the #5 light sensor to analogread pin 28 
-  analogLightLeft = analogRead(A2);     // attaches the #4 light sensor to analogread pin 27
-  analogDistFront = analogRead(A4);     // attaches the front distance sensor to analogread pin 26
+  analogLightRight = analogRead(A2);    // attaches the #5 light sensor to analogread pin 28 
+  analogLightLeft = analogRead(A4);     // attaches the #4 light sensor to analogread pin 27
+  analogDistFront = analogRead(A0);     // attaches the front distance sensor to analogread pin 26
 
-    switch (1) {
+//    switch (1) {
       
-      case 1:
+//      case 1:
         delay(50);          // delay to allow power spike to pass
         left.write(180);    // set left motor to full forward
         right.write(0);     // set right motor to full forward
-        delay(750);         //move for .75 sec
+        delay(500);         //move for .5 sec
 
-      case 2:
+//      case 2:
         time1 = millis();                                   // take the time when case 2 starts
+       
         do
         {
           time2 = millis();                                 // take the time at the start of each loop
-          if(time2 - time1 < 1000, time2 - time1 > 750){    // between .75 sec and 1 sec after case 2 begins lower the front gate
-            gate.write(0);
-            
-          }
+          analogLightRight = analogRead(A2);
+          analogLightLeft = analogRead(A4);
+          analogDistFront = analogRead(A0);
+//          if(time2 - time1 < 500 and time2 - time1 > 250){    // between .75 sec and 1 sec after case 2 begins lower the front gate
+//            gate.write(0);
+//            
+//          }
           if(analogLightRight < analogLightLeft){           // fast line follow code
             left.write(150);
             right.write(0);
@@ -53,13 +74,16 @@ void loop() {
               left.write(180);
               right.write(30);
           }
-        } while(analogDistFront < 2000);                    // loop until front distance reads wall is 10cm away
-         
-      case 3:
+        } while(analogDistFront < 900);                    // loop until front distance reads wall is 10cm away
+
+//      case 3:
         time3 = millis();                             // take the time when case 3 starts
         do
         {
           time4 = millis();                           // take the time at the start of each loop
+          analogLightRight = analogRead(A2);
+          analogLightLeft = analogRead(A4);
+          gate.write(0);
           if(analogLightRight < analogLightLeft){     // slow line follow code
             
             left.write(100);
@@ -69,13 +93,15 @@ void loop() {
               left.write(140);
               right.write(80);
           }
-        } while(time4 - time3 < 2500);                // loop for 2.5 sec after case 3 starts
+        } while(time4 - time3 < 2000);                // loop for 2.5 sec after case 3 starts
         
-      case 4:
+//      case 4:
         time5 = millis();                             // take the time when case 4 starts
         do
         {
           time6 = millis();                           // take the time at the start of each loop
+          analogLightRight = analogRead(A2);
+          analogLightLeft = analogRead(A4);
           if(analogLightRight < analogLightLeft){     // fast line follow code
             
             left.write(150);
@@ -86,5 +112,5 @@ void loop() {
               right.write(30);
           }
         } while(time6 - time5 < 10000000000000);      // follow the line for a long time
-  }
+//  }
 }
---
+++ project.ino
@@ -0,0 +1,81 @@
+#include <Servo.h>
+
+// Globals
+unsigned long sysTime;  // system time
+// Note: writing 0 to right motor moves it forward and
+//       writing 0 to left motor moves it backwards
+//       writing 180 to right motor moves it backwards
+//       writing 180 to left motor moves it forward
+//       writing 90 to either motor does not move it at all
+Servo right;    // create servo object to control right servo 
+Servo left;     // create servo object to control left servo
+Servo gate;     // create servo object to control front gate
+int ls4;        // light sensor 4; connected to pin A2
+int ls5;        // light sensor 5; connected to pin A4
+int dsf;        // distance sensor (front); connected to pin A0
+bool b = true;
+
+void setup() {
+  // put your setup code here, to run once:
+  right.attach(24);   // attaches the right servo to pin 24 
+  left.attach(23);    // attaches the left servo to pin 23
+  //gate.attach(25);    // attaches the front gate to pin 25
+  //lineFollow();
+}
+
+/*
+  Drops the back gate at the
+  beginning
+*/
+void dropBackGate(){
+  
+}
+
+/*
+  Walks forward for half a second
+  to begin line follow. Robot goes
+  outside the start position (the
+  square).
+*/
+void first(){
+  left.write(180);    // moves left motor forward
+  right.write(0);     // moves right motor forward
+  delay(500);         // moves for .5 sec
+  left.write(90);
+  right.write(90);
+}
+
+/*
+  Line follows
+*/
+void lineFollow(){
+  do{
+  
+  sysTime = millis();     // sets the system time
+  ls4 = analogRead(A2);   // read value from light sensor 4
+  ls5 = analogRead(A4);   // read value from light sensor 5
+  dsf = analogRead(A0);   // read value from distance sensor (right)
+  if(ls4 < ls5){
+            left.write(150);
+            right.write(0);
+            
+  }else{
+    left.write(180);
+    right.write(30);
+  }
+
+  }while(dsf < 900);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  if(b){
+    delay(50);          // allows power spike to pass
+    first();            // robot leaves the square
+    b = false;
+  }else{
+    left.write(90);
+    right.write(90);
+  }
+}
