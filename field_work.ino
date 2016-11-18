436335207978a384b8e10d3f58b0ebd3247b0f42
mur-g <mlgeorgeson@utexas.edu>
2016-11-18 17:34:05 -0600

    Now with reset

--- field_work.ino
+++ field_work.ino
@@ -78,7 +78,7 @@ void fastLineFollow() {
     }       
 }
 
-void slowLineFollowLeft() {
+void slowLineFollowRight() {
   analogLightRight = analogRead(A2);
   analogLightLeft = analogRead(A4);
   if(analogLightRight < analogLightLeft){     // slow LEFT line follow code        
@@ -91,12 +91,12 @@ void slowLineFollowLeft() {
     }
 }
 
-void slowLineFollowRight() {
+void slowLineFollowLeft() {
   analogLightRight = analogRead(A2);
   analogLightLeft = analogRead(A4);
   if(analogLightLeft < analogLightRight){     // slow RIGHT line follow code        
     left.write(140);                          // needed for taking turns to avoid getting off course
-    right.write(50);                         
+    right.write(80);                         
     }                                         
   else{                                         
     left.write(100);                          
@@ -168,15 +168,6 @@ void greatBall() {
     }       
 }
 
-void zaptosPivot() {
-  right.write(90);                              // stop and raise the gate
-  left.write(90);
-  gate.write(90);
-  right.write(120);                             // pivot to the right for 1.5 seconds
-  left.write(120);
-  delay(1500);
-}
-
 void loop() {
 
   analogLightRight = analogRead(A2);    // attaches the #5 right light sensor to analog pin 2 
@@ -184,67 +175,66 @@ void loop() {
   analogDistFRight = analogRead(A0);    // attaches the front right distance sensor to analog pin 0
   analogDistFCenter = analogRead(A11);  // attaches the front center distance sensor to analog pin 11 
 
-  first();
+  if (/*first go*/) {
+    first();
 
-  do {
-    analogDistFCenter = analogRead(A11);
-    fastLineFollow();
-    } while(analogDistFCenter < 900);             // loop until front distance reads wall is 10cm away
+    do {
+      analogDistFCenter = analogRead(A11);
+      fastLineFollow();
+      } while(analogDistFCenter < 900);             // loop until front distance reads wall is 10cm away
 
-  time1 = millis();                             // take the time
-  do {
-    time2 = millis();                           // take the time at the start of each loop
-    gate.write(0);                              // lower front gate to capture first pokeball
-    slowLineFollow();
-    } while(time2 - time1 < 2000);              // loop for 2.5 sec
+    time1 = millis();                             // take the time
+    do {
+      time2 = millis();                           // take the time at the start of each loop
+      gate.write(0);                              // lower front gate to capture first pokeball
+      slowLineFollow();
+      } while(time2 - time1 < 2000);              // loop for 2.5 sec
 
-  time3 = millis();                             // take the time
-  do {
-    time4 = millis();                           // take the time at the start of each loop
-    fastLineFollow();                            
-    } while(time4 - time3 < 5000);              // follow the line until the robot has passed the junction
+    time3 = millis();                             // take the time
+    do {
+      time4 = millis();                           // take the time at the start of each loop
+      fastLineFollow();                            
+      } while(time4 - time3 < 5000);              // follow the line until the robot has passed the junction
   
-  gate.write(90);                               // raise front gate
+    gate.write(90);                               // raise front gate
   
-  greatBall();                                  // capture the great ball
+    greatBall();                                  // capture the great ball
 
-  right.write(100);
-  left.write(100);                            // pivot right until on the line again
-  delay(1500);                                // ONLY WORKS FOR LEFT SIDE OF FIELD
+    right.write(100);
+    left.write(100);                            // pivot right until on the line again
+    delay(1500);                                // ONLY WORKS FOR LEFT SIDE OF FIELD
   
-  time5 = millis();                             // take the time
-  do {
-    time6 = millis();                           // take the time at the start of each loop
-    slowLineFollow();
-    } while(time6 - time5 < 2000);              // loop for 2.5 sec until the robot finishes the curve
+    time5 = millis();                             // take the time
+    do {
+      time6 = millis();                           // take the time at the start of each loop
+      slowLineFollow();
+      } while(time6 - time5 < 2000);              // loop for 2.5 sec until the robot finishes the curve
   
-  do {
-    analogDistFRight = analogRead(A0);
-    fastLineFollow();                             // will capture zaptos at this time
-    } while(analogDistFRight < 750);              // loop until front distance reads wall is 40cm away after it captures zaptos
+    do {
+      analogDistFRight = analogRead(A0);
+      fastLineFollow();                             // will capture zaptos at this time
+      } while(analogDistFRight < 750);              // loop until front distance reads wall is 40cm away after it captures zaptos
+    }
 
-  zaptosPivot();                                  // pivot away from zaptos while raising gate
-  
-  do {
-    analogDistFRight = analogRead(A0);
-    fastLineFollow();
-    } while(analogDistFRight < 900);            // loop until front distance reads wall is 10cm away
-  
-  time7 = millis();                             // take the time
-  do {
-    time8 = millis();                           // take the time at the start of each loop
-    slowLineFollow();
-    } while(time8 - time7 < 2000);              // loop for 2.5 sec until the robot passes the junction  
-  
-  greatBall();                                  // capture another great ball
-
-  time9 = millis();                              // take the time
-  do {
-    time10 = millis();                            // take the time at the start of each loop
-    slowLineFollow();                             // will capture the corner pokemon at this time
-    } while(time10 - time9 < 2000);               // loop for 2.5 sec until the robot passes the curve
+  if (/*reset*/) {
+    first();
     
-  right.write(90);
-  left.write(90);
-  delay(1000000000000000000);                     // and we're done unless we decide to do something else  
+    time7 = millis();                             // take the time
+    do {
+      time8 = millis();                           // take the time at the start of each loop
+      fastLineFollow();
+      } while(time8 - time7 < 2000);              // loop for 2.5 sec until the robot passes the junction  
+  
+    greatBall();                                  // capture another great ball
+
+    time9 = millis();                              // take the time
+    do {
+      time10 = millis();                            // take the time at the start of each loop
+      slowLineFollow();                             // will capture the corner pokemon at this time
+      } while(time10 - time9 < 2000);               // loop for 2.5 sec until the robot passes the curve
+    
+    right.write(90);
+    left.write(90);
+    delay(1000000000000000000);                     // and we're done unless we decide to do something else  
+  }
 }
