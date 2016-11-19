544ebe876dcdcd969073e88f7b62d347985ef705
SamSelle <sselle@utexas.edu>
2016-11-18 18:16:20 -0600

    Add files via upload

--- Switch_Test.ino
+++ Switch_Test.ino
@@ -1,13 +1,15 @@
-int Value = 2
+int TGLPIN = 10;
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
+pinMode(TGLPIN, INPUT); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
- Value = digitalRead(10);
- Serial.println(Value);
+ int tglOn = digitalRead(TGLPIN);
+ Serial.println(tglOn);
+ delay(100);
  
 }
